f812a8a869ae30b6e41f8b5082cd23cfb4fec60d
anishi1222 <anishi1222@users.noreply.github.com>
2017-01-23 22:54:14 +0900

    Update Remove_HTTPHeader.groovy

--- Remove_HTTPHeader.groovy
+++ Remove_HTTPHeader.groovy
@@ -6,9 +6,9 @@ context.getSouthboundCallout().withoutHeader("X-AAAA")
 // Remove specified HTTP Header in response pipeline
 context.getClientResponse().withoutHeader("X-AAAA")
 
-// Remove all HTTP headers in request pipeline
+// Ignore all HTTP headers in request pipeline
 context.getSouthboundCallout().ignoreAllRequestHeaders(true)
 
-// Remove all HTTP headers in response pipeline
+// Ignore all HTTP headers in response pipeline
 context.getClientResponse().ignoreAllRequestHeaders(true)
 
