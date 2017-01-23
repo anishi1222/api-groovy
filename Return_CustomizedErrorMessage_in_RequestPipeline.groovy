0c879592d6afe602314f28338228f7443e5c4b88
anishi1222 <anishi1222@users.noreply.github.com>
2017-01-23 22:51:08 +0900

    Update Return_CustomizedErrorMessage_in_RequestPipeline.groovy

--- Return_CustomizedErrorMessage_in_RequestPipeline.groovy
+++ Return_CustomizedErrorMessage_in_RequestPipeline.groovy
@@ -4,8 +4,8 @@ def messageBody = ["errorcode": "XXXX"]
 def json = groovy.json.JsonOutput.toJson(messageBody)
 def length = json.toString().length().toString()
 
-// Content-Type is not required when using Content-Type which comes from Backend service
-// Content-Length should be set
+// Content-Type is not required when using Content-Type which comes from API request
+// Content-Length should be set since content-length when modifying a request message.
 context.getClientResponse().withStatus(404).withBodyAsObject(json).withHeader("Content-Length",length).withHeader("Content-Type","application/json")
 
 // The following method is required when subsequent activities in pipeline should not be executed
