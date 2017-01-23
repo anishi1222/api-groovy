5761390ec7e6eb08c2480bfaa6b2634c3d21643c
anishi1222 <anishi1222@users.noreply.github.com>
2017-01-23 22:52:29 +0900

    Update Return_CustomizedErrorMessage_in_ResponsePipeline.groovy

--- Return_CustomizedErrorMessage_in_ResponsePipeline.groovy
+++ Return_CustomizedErrorMessage_in_ResponsePipeline.groovy
@@ -5,8 +5,8 @@ def json = groovy.json.JsonOutput.toJson(messageBody)
 def length = json.toString().length().toString()
 
 // Content-Type is not required when using Content-Type which comes from Backend service
-// Content-Length should be set
+// Content-Length should be set when modifying a response message.
 context.getClientResponse().withStatus(404).withBodyAsObject(json).withHeader("Content-Length",length)
 
-// The following method is required when subsequent activities in pipeline should not be executed
+// The following method is required when you don't want subsequent activities in pipeline to be executed.
 context.forceResponse()
