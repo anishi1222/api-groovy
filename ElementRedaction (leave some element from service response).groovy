555ce7eb4afbee92b95aaa5164d0a2cc73a40f30
anishi1222 <anishi1222@users.noreply.github.com>
2017-01-28 10:23:31 +0900

    Create ElementRedaction (leave some element from service response).groovy

---
+++ ElementRedaction (leave some element from service response).groovy
@@ -0,0 +1,29 @@
+// Leave some element of response message received from backend service.
+// Redaction policy is much easier than this script. We strongly recommend using the Redaction policy.
+// 
+// Original message 
+//  {
+//    "account_number": "4859634",
+//    "name": "Sam",
+//    "gas_usage": "20,005 Btu",
+//    "gas_cost": "¥ 46,896",
+//    "electricity_usage": "3,430 kW",
+//    "electricity_cost": "¥ 12,932",
+//    "environment": "Japan 1",
+//    "serviceID": "7777"
+//  }
+//
+
+// Retrieve the element "serviceID" from service response
+def body = context.getSouthboundResponse().getBodyAsType(String.class)
+def slurper = new groovay.json.JsonSlurper()
+
+def messageBody = (Map)slurper.parseText(body)
+def newMessage  = (Map)[:]
+
+newMessage.add("serviceID", messageBody["serviceID"])
+
+def json = groovy.json.JsonOutput.toJson(newMessage)
+def length = json.toString().length().toString()
+
+context.getClientResponse().withBodyAsObject(json).withHeader("Content-Length",length)
