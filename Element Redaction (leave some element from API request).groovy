ad11f0e19b4d8f2699a7fb951b1ef4a5b370ba96
anishi1222 <anishi1222@users.noreply.github.com>
2017-01-28 18:46:02 +0900

    Create Element Redaction (leave some element from API request).groovy

---
+++ Element Redaction (leave some element from API request).groovy
@@ -0,0 +1,29 @@
+// Leave some element only of API request and pass it to backend service.
+// Redaction policy is much easier than this script. We strongly recommend using the Redaction policy.
+// 
+// Original message 
+//  {
+//    "account_number": "4859634",
+//    "name": "Sam",
+//    "gas_usage": "20,005 Btu",
+//    "gas_cost": "¥ 46,896",
+//    "electricity_usage": "3,430 kW",
+//    "electricity_cost": "¥ 12,932",
+//    "environment": "Japan 1",
+//    "serviceID": "7777"
+//  }
+//
+
+// Retrieve the element "serviceID" from service response
+def body = context.getClientRequest().getBodyAsType(String.class)
+def slurper = new groovy.json.JsonSlurper()
+
+def messageBody = (Map)slurper.parseText(body)
+def newMessage  = (Map)[:]
+
+newMessage.put("serviceID", (String)messageBody["serviceID"])
+
+def json = groovy.json.JsonOutput.toJson((Map)newMessage)
+def length = json.toString().length().toString()
+
+context.getSouthboundCallout().withBodyAsObject(json).withHeader("Content-Length",length)
