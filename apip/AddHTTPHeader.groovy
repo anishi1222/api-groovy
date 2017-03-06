d025217f02bc96a8200d6959885d3b4c5f7f9de9
anishi1222 <anishi1222@users.noreply.github.com>
2017-03-06 09:23:39 +0900

    Create AddHTTPHeader.groovy

---
+++ apip/AddHTTPHeader.groovy
@@ -0,0 +1,5 @@
+// Add HTTP Header to Service Request
+context.serviceRequest.setHeader("X-ServiceRequest", "XXXX")
+
+// Add HTTP Header to API Response
+Context.apiResponse.setHeader("X-API_Response", "XXXX")
