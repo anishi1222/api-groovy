0e20353c1ad5de702c8849a5bbc3b96c211f57cd
anishi1222 <anishi1222@users.noreply.github.com>
2017-03-06 10:04:01 +0900

    Rename RemoveElementsFromJSONList.groovy to RemoveElementsFromJSONListInServiceResponse.groovy

---
+++ apip/RemoveElementsFromJSONListInServiceResponse.groovy
@@ -0,0 +1,8 @@
+// Retrieve the element "serviceID" from service response
+def newMessage  = (Map)[:]
+newMessage.put("serviceID", (String)context.serviceResponse.getBody().asJSONObject().get("serviceID"))
+
+def json = groovy.json.JsonOutput.toJson((Map)newMessage)
+def newBody = new StringBodyImpl(json.toString(), "application/json")
+def length = newBody.asString().length().toString()
+context.apiResponse.setBody(newBody).setHeader("Content-Length",length)
