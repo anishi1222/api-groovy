baa96bda1945e82e3afa422f2f79b624579a287e
anishi1222 <anishi1222@users.noreply.github.com>
2017-03-17 11:22:26 +0900

    Create LeaceElementsFromJSONList.groovy

---
+++ ocsg/LeaceElementsFromJSONList.groovy
@@ -0,0 +1,40 @@
+// Elements which contains the value of "MARINE" as "category" are extracted from the following original JSON message
+// 
+// {
+//     "products": [
+//         {
+//             "pid": "AUTO-201",
+//             "base_info": {
+//                 "category": "AUTO",
+//                 "product_name": "AAAA"
+//             }
+//         },
+//         {
+//             "pid": "AUTO-123",
+//             "base_info": {
+//                 "category": "AUTO",
+//                 "product_name": "BBBB"
+//             }
+//         },
+//         {
+//             "pid": "MARINE-100",
+//             "base_info": {
+//                 "category": "MARINE",
+//                 "product_name": "CCCC"
+//             }
+//         }
+//     ]
+// }
+
+def body = context.getSouthboundResponse().getBodyAsType(String.class)
+def slurper = new groovy.json.JsonSlurper()
+
+def messageBody = (Map)slurper.parseText(body)
+def products = (List)messageBody.products
+
+messageBody.products = products.findAll{it["base_info"]["category"] == "MARINE" }
+
+def json = groovy.json.JsonOutput.toJson(messageBody)
+def length = json.toString().length().toString()
+
+context.getClientResponse().withBodyAsObject(json).withHeader("Content-Length",length) 
