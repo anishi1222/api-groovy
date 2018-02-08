c154fe7eb8be31ff7794e2de0a3c920656c17b26
anishi1222 <anishi1222@users.noreply.github.com>
2018-02-08 18:24:23 +0900

    Update AddDefaultQueryParameter.groovy

--- apip/AddDefaultQueryParameter.groovy
+++ apip/AddDefaultQueryParameter.groovy
@@ -1,11 +1,11 @@
 def key = 'pid'      // QueryParameterのkey
 def val = 'AUTO-123' // QueryParameterのValue
-def queryParams = (Map)context.apiRequest.getQueryParams()
+def queryParams = (Map)context.ApiRequest.getQueryParams()
 
 // No operation when default query parameters are specified
 if( queryParams != null && queryParams.containsKey(key) != null) {
 }
 else {
     // Add default query parameters unless default query parameters are not specified
-    context.serviceRequest.setQueryParam(key, val)
+    context.ServiceRequest.setQueryParam(key, val)
 }
