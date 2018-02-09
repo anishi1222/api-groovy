ce7e1083803dc8dce293abb422d7d31ee630cd73
anishi1222 <anishi1222@users.noreply.github.com>
2018-02-09 11:07:13 +0900

    Update ReturnCustomizedErrorMessageInResponsePipeline.groovy

--- apip/ReturnCustomizedErrorMessageInResponsePipeline.groovy
+++ apip/ReturnCustomizedErrorMessageInResponsePipeline.groovy
@@ -2,12 +2,12 @@
 def body = new StringBodyImpl("{\"errorcode\": \"XXXX\"}", "application/json")
 def length = body.asString().length().toString()
 
-context.apiResponse.setStatus(404).setBody(body).setHeader("Content-Length",length).setHeader("Content-Type","application/json")
+context.ApiResponse.setStatus(404).setBody(body).setHeader("Content-Length",length).setHeader("Content-Type","application/json")
 
 // Error Response (2)
 def messageBody = ["errorcode": "XXXX"]
 def json = groovy.json.JsonOutput.toJson(messageBody)
 def errorId = "XXXX"
-context.apiResponse.setHeader("Content-Type","application/json")
+context.ApiResponse.setHeader("Content-Type","application/json")
 
 throw new oracle.apiplatform.policies.sdk.exceptions.PolicyProcessingException(errorId, 400, json.toString())
