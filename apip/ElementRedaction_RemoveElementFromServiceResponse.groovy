eabaf8a7bec48c92e17034838d31f37dc46074fd
anishi1222 <anishi1222@users.noreply.github.com>
2018-02-09 11:02:46 +0900

    Update ElementRedaction_RemoveElementFromServiceResponse.groovy

--- apip/ElementRedaction_RemoveElementFromServiceResponse.groovy
+++ apip/ElementRedaction_RemoveElementFromServiceResponse.groovy
@@ -1,6 +1,6 @@
-def body = context.serviceResponse.getBody()
+def body = context.ServiceResponse.getBody()
 body.asJSONObject().remove("serviceID")
 
 def length = body.asString().length().toString()
 
-context.apiResponse.setBody(body).setHeader("Content-Length",length)
+context.ApiResponse.setBody(body).setHeader("Content-Length",length)
