23d603ea39425002ebd89a667e03ef136d7c2772
anishi1222 <anishi1222@users.noreply.github.com>
2018-02-09 10:57:04 +0900

    Update ModifyQueryParameterValue.groovy

--- apip/ModifyQueryParameterValue.groovy
+++ apip/ModifyQueryParameterValue.groovy
@@ -4,7 +4,7 @@ def newVal = 'MARINE'
 def queryParams = (Map)context.apiRequest.getQueryParams()
 
 // Change value of query parameter in case of meeting conditions.
-if( queryParams != null && queryParams.containsKey(targetKey) != null) {
+if( queryParams.isEmpty() || !queryParams.containsKey(targetKey)) {
     def val = (String)((List)queryParams.get(targetKey)).get(0);
     if(val == prevVal) {
         context.serviceRequest.setQueryParam(targetKey, newVal)
