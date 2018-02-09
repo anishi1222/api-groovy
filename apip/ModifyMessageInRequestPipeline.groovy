c50a69c30a179fa94426882ffb3b4146661cff12
anishi1222 <anishi1222@users.noreply.github.com>
2018-02-09 11:03:06 +0900

    Update ModifyMessageInRequestPipeline.groovy

--- apip/ModifyMessageInRequestPipeline.groovy
+++ apip/ModifyMessageInRequestPipeline.groovy
@@ -1,6 +1,6 @@
-def body = context.apiRequest.getBody()
+def body = context.ApiRequest.getBody()
 body.asJSONObject().put("serviceID", "XXXX")
 
 def length = body.asString().length().toString()
 
-context.serviceRequest.setBody(body).setHeader("Content-Length",length)
+context.ServiceRequest.setBody(body).setHeader("Content-Length",length)
