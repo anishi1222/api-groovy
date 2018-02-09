1a42f1f70f258a7216f794a8594031593d09f778
anishi1222 <anishi1222@users.noreply.github.com>
2018-02-09 11:04:01 +0900

    Update ModifyQueryParameterKey.groovy

--- apip/ModifyQueryParameterKey.groovy
+++ apip/ModifyQueryParameterKey.groovy
@@ -1,12 +1,11 @@
 def prevKey = 'dip'
 def newKey = 'pid'
-def queryParams = (Map)context.apiRequest.getQueryParams()
+def queryParams = (Map)context.ApiRequest.getQueryParams()
 
 // Change key of query parameter when target parameter is set
 if( queryParams.isEmpty() || !queryParams.containsKey(prevKey)) {
     def val = (String)((List)queryParams.get(prevKey)).get(0);
-    context.serviceRequest.withoutQueryParam(prevKey)
-    context.serviceRequest.setQueryParam(newKey, val)
+    context.ServiceRequest.withoutQueryParam(prevKey).setQueryParam(newKey, val)
 }
 else {
     // No operation when no query parameter is set or specified parameter is not target one.
