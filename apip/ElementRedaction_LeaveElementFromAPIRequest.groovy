0c5d23df85ae12379360a0e2dc56293afe0d1efa
anishi1222 <anishi1222@users.noreply.github.com>
2018-02-09 11:01:27 +0900

    Update ElementRedaction_LeaveElementFromAPIRequest.groovy

--- apip/ElementRedaction_LeaveElementFromAPIRequest.groovy
+++ apip/ElementRedaction_LeaveElementFromAPIRequest.groovy
@@ -1,8 +1,8 @@
 // Retrieve the element "serviceID" from API Request
 def newMessage  = (Map)[:]
-newMessage.put("serviceID", (String)context.apiRequest.getBody().asJSONObject().get("serviceID"))
+newMessage.put("serviceID", (String)context.ApiRequest.getBody().asJSONObject().get("serviceID"))
 
 def json = groovy.json.JsonOutput.toJson((Map)newMessage)
 def newBody = new StringBodyImpl(json.toString(), "application/json")
 def length = newBody.asString().length().toString()
-context.apiResponse.setBody(newBody).setHeader("Content-Length",length)
+context.ApiResponse.setBody(newBody).setHeader("Content-Length",length)
