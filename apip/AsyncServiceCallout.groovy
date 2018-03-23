a79c29eb6b621f04f4aad03e20de1df42469adfd
anishi1222 <anishi1222@users.noreply.github.com>
2018-03-23 10:03:13 +0900

    Create AsyncServiceCallout.groovy

---
+++ apip/AsyncServiceCallout.groovy
@@ -0,0 +1,26 @@
+// This is supported in 18.1.5 or after
+
+ExternalServiceCallout callout = context.newCallout()
+ExternalServiceCallout.Callback callback = new ExternalServiceCallout.Callback() {
+ 
+        boolean onCompleted(ServiceResponse response)
+            throws PolicyProcessingException {
+ 
+            System.out.println("-----> onCompleted()");
+            System.out.println("Response: " + response.getBody().asString());
+            return true;
+ 
+        }
+ 
+        boolean onThrowable(Throwable throwable)
+            throws PolicyProcessingException {
+ 
+            System.out.println("-----> onThrowable()");
+            return true;
+ 
+       }
+}
+
+// Configure URL and Method as you like
+callout.setRequestURL("URL").setMethod("Mehthod");
+callout.sendAsync(callback);
