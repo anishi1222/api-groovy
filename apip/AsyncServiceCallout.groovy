225ec8a051e2375a47860ca1b245f220d628d824
anishi1222 <anishi1222@users.noreply.github.com>
2018-03-23 11:43:59 +0900

    Update AsyncServiceCallout.groovy

--- apip/AsyncServiceCallout.groovy
+++ apip/AsyncServiceCallout.groovy
@@ -3,15 +3,24 @@ ExternalServiceCallout.Callback callback = new ExternalServiceCallout.Callback()
 
     boolean onCompleted(ServiceResponse response) throws PolicyProcessingException {
         println "Response: " + response.getBody().asString()
-        // If response is used later, response should be stored using setAttribute() or setEdr() 
+        // If you want to use the response later, store the response using setAttribute() or setEdr() 
+        context.setAttribute("response1", response.getBody())
         return true
     }
 
     boolean onThrowable(Throwable throwable) throws PolicyProcessingException {
-        return true;
+        return true
     }
 }
 
 // As ExternalServiceCallout extends ServiceRequest, you can invoke any APIs even if request message is mandatory.
 callout.setRequestURL("API URL").setMethod("HTTP Method")
 callout.sendAsync(callback)
+
+/* -----------------------------------
+ *
+ * Substitute groovy script policy...
+ *
+ * -----------------------------------*/
+Body calloutBody = (Body)context.getAttribute("response1")
+println calloutBody.getString()
