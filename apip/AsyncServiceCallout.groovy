f492d74214a658c4c628df20cc97c85d7c51b5da
anishi1222 <anishi1222@users.noreply.github.com>
2018-03-23 11:06:02 +0900

    Update AsyncServiceCallout.groovy

--- apip/AsyncServiceCallout.groovy
+++ apip/AsyncServiceCallout.groovy
@@ -1,14 +1,14 @@
 ExternalServiceCallout callout = context.newCallout()
 ExternalServiceCallout.Callback callback = new ExternalServiceCallout.Callback() {
- 
-        boolean onCompleted(ServiceResponse response) throws PolicyProcessingException {
-            println "Response: " + response.getBody().asString()
-            return true
-        }
- 
-        boolean onThrowable(Throwable throwable) throws PolicyProcessingException {
-            return true;
-       }
+
+    boolean onCompleted(ServiceResponse response) throws PolicyProcessingException {
+        println "Response: " + response.getBody().asString()
+        return true
+    }
+
+    boolean onThrowable(Throwable throwable) throws PolicyProcessingException {
+        return true;
+    }
 }
 
 // As ExternalServiceCallout extends ServiceRequest, you can invoke any APIs even if request message is mandatory.
