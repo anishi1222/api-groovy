343ff42718ed6a9b37c5e893ddebf4fa15935e49
Akihiro Nishikawa <anishi1222@users.noreply.github.com>
2018-06-07 19:53:33 +0900

    Update ReturnCustomizedErrorMessageInResponsePipeline.groovy

--- apip/ReturnCustomizedErrorMessageInResponsePipeline.groovy
+++ apip/ReturnCustomizedErrorMessageInResponsePipeline.groovy
@@ -1,13 +1,16 @@
 // Error Response (1)
-def body = new StringBodyImpl("{\"errorcode\": \"XXXX\"}", "application/json")
+// In case of using ordinal API response...
+def messageBody = ["errorcode": "XXXX"]
+def json = groovy.json.JsonOutput.toJson(messageBody)
+def body = new StringBodyImpl(json.toString(), "application/json")
 def length = body.asString().length().toString()
 
-context.ApiResponse.setStatus(404).setBody(body).setHeader("Content-Length",length).setHeader("Content-Type","application/json")
+context.ApiResponse.setStatusCode(400).setBody(body).setHeader("Content-Length",length).setHeader("Content-Type","application/json")
 
 // Error Response (2)
+// In case of using PolicyProcessingException...
 def messageBody = ["errorcode": "XXXX"]
 def json = groovy.json.JsonOutput.toJson(messageBody)
 def errorId = "XXXX"
-context.ApiResponse.setHeader("Content-Type","application/json")
-
-throw new oracle.apiplatform.policies.sdk.exceptions.PolicyProcessingException(errorId, 400, json.toString())
+context.ApiResponse.setBody(body).setHeader("Content-Length",length).setHeader("Content-Type","application/json")
+throw new oracle.apiplatform.policies.sdk.exceptions.PolicyProcessingException(errorId, 400, "XXXX")
